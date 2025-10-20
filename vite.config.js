0341c2a6b4d6208b65a3ca69b4a64fbea4592e9f
Dxd5642 <dxd56422@yandex.ru>
2025-10-20 14:58:26 +0500

    sa

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/UltraDentDeploy/',
   server: {
     host: '0.0.0.0',
     port: 5642, 
     strictPort: false
-  }, 
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+  }
 })

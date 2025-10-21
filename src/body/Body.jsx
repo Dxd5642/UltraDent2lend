adc35860a165f94869da7c6fcd60633c756cdf3c
Dxd5642 <dxd56422@yandex.ru>
2025-10-21 21:04:14 +0500

    Создан и адаптирован 2 блок

--- src/body/Body.jsx
+++ src/body/Body.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './body.scss';
 import Hero from '../hero/Hero';
+import Bottom1 from '../bottom1/Bottom1';
 
 function Body (){
     return(
         <div className='body-parent'>
             <Hero />
+            <Bottom1 />
         </div>
     )
 }
